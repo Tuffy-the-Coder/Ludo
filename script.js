1059109323460f1e0a44a61537e78a31d644f954
Anurag <Rawat123anurag@gmail.com>
2025-05-15 15:33:48 +0530

    memes-added

---
+++ script.js
@@ -0,0 +1,24 @@
+
+// playng memes
+
+document.querySelectorAll(".meme").forEach((e) => {
+    e.addEventListener("mousedown", (event) => {
+        event.preventDefault();
+        if (e.id == "dog-smile-meme") {
+            e.src = "images/dog-smile.gif";
+        }
+        else if (e.id == "sad-meme") {
+            e.src = "images/sad.gif";
+        }
+    })
+    document.addEventListener("mouseup", () => {
+        if (e.id == "dog-smile-meme") {
+            e.src = "images/dog-smile.png";
+            
+        }
+        else if (e.id == "sad-meme") {
+            e.src = "images/sad.png";
+        }
+    })
+
+})
